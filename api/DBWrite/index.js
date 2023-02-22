dfeb251723014459a8eb49176ce6b9af7159a3c9
Ole Kristian Settem <ole@zmmaskin.no>
2023-02-22 12:47:06 +0100

    Initial

---
+++ api/DBWrite/index.js
@@ -0,0 +1,33 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const firstName = (req.query.firstname || (req.body && req.body.firstname));
+    const lastName = (req.query.lastname || (req.body && req.body.lastname));
+    const email = (req.query.email || (req.body && req.body.email));
+    const phone = (req.query.phone || (req.body && req.body.phone));
+    const role = (req.query.role || (req.body && req.body.role));
+    const createdDate = (req.query.createddate || (req.body && req.body.createddate));
+    const deactivatedDate = (req.query.deactivateddate || (req.body && req.body.deactivateddate));
+    const status = (req.query.status || (req.body && req.body.status));
+        const responseMessage = firstName + " " + lastName
+        ? "Success"
+        : "This HTTP triggered function executed successfully.";
+    context.bindings.outputDocument = JSON.stringify({
+            // create a random ID
+            id: new Date().toISOString() + Math.random().toString().substring(2, 10),
+            name: lastName + ", " + firstName,
+            firstname: firstName,
+            lastname: lastName,
+            email: email,
+            phone: phone,
+            role: role,
+            createddate: createdDate,
+            deactivateddate: deactivatedDate,
+            status: status
+        });
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
