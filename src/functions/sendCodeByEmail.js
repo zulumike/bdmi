39c4ccf7d27afbe3792575f093664f568b38bb98
Ole Kristian Settem <ole@zmmaskin.no>
2023-03-06 22:43:29 +0100

    delete unused/old code

--- src/functions/sendCodeByEmail.js
+++ src/functions/sendCodeByEmail.js
@@ -26,18 +26,6 @@ async function sendCodeByEmail(mailAddress, code) {
         body: JSON.stringify(messageData)
     });
     return (responseMessage);
-
-
-    // let messageData = {};
-    // let xhr = new XMLHttpRequest();
-    // xhr.open("POST", apiURL);
-    // xhr.setRequestHeader("Content-Type", "application/json");
-    // xhr.onload = () => console.log('sendEmail api status: ', xhr.responseText);
-    // messageData.mailAddress = mailAddress;
-    // messageData.subject = 'Velkommen til Bevar Dovrefjell';
-    // messageData.text = 'Tast inn følgende kode for å fullføre registreringen: ' + code;
-    // let data = JSON.stringify(messageData);
-    // xhr.send(data);
 };
 
 export default sendCodeByEmail;
