b816b0c10e7b942b53806ddb36076c20bbbb6b32
zulumike <ole@zmmaskin.no>
2023-07-11 12:53:48 +0200

    Family register OK

--- src/components/FamilyMembers.js
+++ src/components/FamilyMembers.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import updateMember from "../functions/updateMember";
 
 
@@ -10,11 +10,26 @@ function FamilyMembers({member}) {
         const value = event.target.value;
         setFormInputs(values => ({...values, [name]: value}))
     };
-    console.log(member.family);
-    const testData = Object.entries(member.family);
-    testData.map((members, index) => console.log(members))
     
-    async function submitForm(event) {
+    const [ familyData, setFamilyData ] = useState([]);
+    useEffect(() => {
+        if (member.family) {
+            setFamilyData(Object.values(member.family));
+        }
+        else {
+            setFamilyData([]);
+    
+        };
+    }, []
+    );
+
+    const familyChange = (index, event) => {
+        let data = [...familyData];
+        data[index][event.target.name] = event.target.value;
+        setFamilyData(data);
+    };
+
+    function submitForm(event) {
         event.preventDefault();
         const familyMemberId = new Date().toISOString() + Math.random().toString().substring(2, 10);
         const memberData = member;
@@ -22,13 +37,31 @@ function FamilyMembers({member}) {
             memberData.family = {};
         };
         memberData.family[familyMemberId] = formInputs;
-        console.log(memberData);
-        const writeResult = await updateMember(member.id, memberData);
-        if (writeResult.status !== 200) alert('Lagring feilet! Feilmelding: ', writeResult.statusText);
+        member = memberData;
+        setFamilyData(Object.values(memberData.family));
         setFormInputs({});
-        // document.location.reload();
+    };
+    
+    function deleteFamilyMember(index, event) {
+        event.preventDefault();
+        const familyDataCopy = familyData.slice(0, index).concat(familyData.slice(index+1));
+        console.log(member.family);
+        console.log(familyDataCopy);
+        setFamilyData(familyDataCopy);
+
+        const deleteKey = Object.keys(member.family)[index];
+        delete member.family[deleteKey];
+        console.log(deleteKey)
+
+        console.log(member.family);
     };
 
+    async function saveFamily(event) {
+        event.preventDefault();
+        const writeResult = await updateMember(member.id, member);
+        if (writeResult.status !== 200) alert('Lagring feilet! Feilmelding: ', writeResult.statusText);
+        document.location.reload();
+    };
 
     return (
         <div>
@@ -85,7 +118,55 @@ function FamilyMembers({member}) {
                 <input type="submit" value="Legg til" />
             </form>
             <br/>
-            
+            <form>
+                {familyData.map((familyMemberData, index) => {
+                    return (
+                        <div key={index}>
+                            <button className="deletebtn" onClick={event => deleteFamilyMember(index, event)}>Slett</button>
+                            <input
+                            name='firstname'
+                            placeholder='Fornavn'
+                            required
+                            value={familyMemberData.firstname}
+                            onChange={event => familyChange(index, event)}
+                            />
+                            <input
+                            name='lastname'
+                            placeholder='Etternavn'
+                            required
+                            value={familyMemberData.lastname}
+                            onChange={event => familyChange(index, event)}
+                            />
+                            <input
+                            type="email"
+                            name='email'
+                            placeholder='E-post'
+                            value={familyMemberData.email}
+                            onChange={event => familyChange(index, event)}
+                            />
+                            <input
+                            type="tel"
+                            name='phone'
+                            placeholder='Mobilnummer'
+                            pattern="[0-9]{8}"
+                            value={familyMemberData.phone}
+                            onChange={event => familyChange(index, event)}
+                            />
+                            <input
+                            name='zipcode'
+                            placeholder='Postnr'
+                            pattern="\d{4}"
+                            required
+                            value={familyMemberData.zipcode}
+                            onChange={event => familyChange(index, event)}
+                            />
+                        </div>
+                    )
+                } )}
+                <br/>
+                <button className="centerbtn" onClick={saveFamily}>Lagre</button>
+                <br/>
+            </form>
         </div>
     )
 }
