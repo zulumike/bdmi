b5550a7513b42027124e3f9909edfc43f5fc5b96
zulumike <ole@zmmaskin.no>
2024-01-23 14:00:59 +0100

    Created own code-dialog for memberform

---
+++ src/components/LoginPrompt.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import writeNewMember from "../functions/writeNewMember";
+
+function LoginPrompt({codeToCheck, memberData}) {
+
+    const [ data, setData ] = useState('');
+
+    function dataChange(event) {
+        setData(event.target.value);
+    }
+
+    function submitData() {
+
+        if (codeToCheck === parseInt(data)) {
+            memberData.id = new Date().toISOString() + Math.random().toString().substring(2, 5);
+            memberData.createdby = memberData.email;
+            memberData.role = "Medlem";
+            memberData.status = "Registrert";
+            memberData.token = new Date().toISOString() + Math.random().toString().substring(2, 10);
+            const writeResult = writeNewMember(memberData);
+            writeResult.then((responseMessage) => {
+                if (responseMessage.status === 200) {
+                    localStorage.setItem('user', JSON.stringify({username: memberData.email, userrole: memberData.role, token: memberData.token }));
+                    alert('Velkommen til Bevar Dovrefjell Mellom Istidene');
+                    window.location.reload(false);
+                }
+                else {
+                    alert('Lagring feilet! Feilmelding: ', responseMessage.statusText);    
+                }
+                    });
+        };
+    };
+
+
+    return (
+        <div>
+            <h2>Skriv inn tilsendt kode</h2>
+            <input type="text" value={data} onChange={dataChange} />
+            <button onClick={submitData}>OK</button>
+        </div>
+    )
+};
+
+export default LoginPrompt;
--- src/components/MemberForm.js
+++ src/components/MemberForm.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import ReactModal from "react-modal";
+import Modal from "react-modal";
 import checkIfMemberExist from "../functions/checkIfMemberExist";
 import sendCodeByEmail from "../functions/sendCodeByEmail";
-import writeNewMember from "../functions/writeNewMember";
 import '../styles/default.css';
 import salgsbetingelser from "../assets/Salgsbetingelser.pdf";
+import LoginPrompt from "./LoginPrompt";
 
 
 //*******************
@@ -21,6 +23,16 @@ function MemberForm() {
         setFormInputs(values => ({...values, [name]: value}))
     };
 
+    const randomCode = useRef(Math.floor(Math.random()*999999)+100001);
+
+    Modal.setAppElement('#root');
+    const [ modalOpen, setModalOpen ] = useState(false);
+
+    function closeDialog() {
+        setModalOpen(false);
+    };
+
+
     //********************
     // FUNCTION submitForm
     // Creates random code, sends code by email
@@ -29,45 +41,12 @@ function MemberForm() {
 
     async function submitForm(event) {
         event.preventDefault();
-        const randomCode = Math.floor(Math.random()*999999)+100001;
+        // const randomCode = Math.floor(Math.random()*999999)+100001;
         const [memberExist, phoneOrEmail] = await checkIfMemberExist(formInputs.phone, formInputs.email);
         if (memberExist) alert(phoneOrEmail + ' er registert fra før!\nPrøv en annen eller kontakt post@bevardovrefjell.no')
         else {
-            sendCodeByEmail([formInputs.email], randomCode);
-            let abort = false;
-            while (abort === false) {
-                let userCode = "";
-                while (userCode === "") {
-                    userCode = prompt("Kode er nå sendt på e-post.\nSkriv inn tilsendt kode her.\nHvis ikke mottatt, sjekk spam.\nTrykk evt avbryt og send inn på nytt");
-                    console.log(userCode);
-                };
-                if (parseInt(userCode) === randomCode) {
-                    abort = true;
-                    formInputs.id = new Date().toISOString() + Math.random().toString().substring(2, 5);
-                    formInputs.createdby = formInputs.email;
-                    formInputs.role = "Medlem";
-                    formInputs.status = "Registrert";
-                    formInputs.token = new Date().toISOString() + Math.random().toString().substring(2, 10);
-                    const writeResult = writeNewMember(formInputs);
-                    writeResult.then((responseMessage) => {
-                        if (responseMessage.status === 200) {
-                            localStorage.setItem('user', JSON.stringify({username: formInputs.email, userrole: formInputs.role, token: formInputs.token }));
-                            alert('Velkommen til Bevar Dovrefjell Mellom Istidene');
-                            setFormInputs({});
-                            window.location.reload(false);
-                        }
-                        else {
-                            alert('Lagring feilet! Feilmelding: ', responseMessage.statusText);    
-                        }
-                         });
-                }
-                else if (userCode === null) {
-                    abort = true;
-                }
-                else {
-                    alert("Feil kode");
-                }   
-            };
+            sendCodeByEmail([formInputs.email], randomCode.current);
+            setModalOpen(true);
         };
     };
 
@@ -155,6 +134,17 @@ function MemberForm() {
                 samt kreve inn kontingent. <a href = {salgsbetingelser} target = "_blank" rel="noreferrer">Salgsbetingelser</a>
             </p>
             <h2>Kontakt: post@bevardovrefjell.no</h2>
+            <ReactModal 
+                className='modal'
+                ovarlayClassName='modaloverlay'
+                isOpen={modalOpen}
+                onRequestClose={closeDialog}
+                shouldCloseOnOverlayClick={false}
+                shouldCloseOnEsc={false}
+                >
+                <LoginPrompt codeToCheck={randomCode.current} memberData={formInputs}/>
+                <button onClick={closeDialog}>Avbryt</button>
+            </ReactModal>
         </div>
     )
 }
