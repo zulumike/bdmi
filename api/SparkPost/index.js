51790dbac4543eac371a468a93dfb8d72dbfd2cd
zulumike <ole@zmmaskin.no>
2024-01-28 21:37:11 +0100

    Fixed attachements in email

--- api/SparkPost/index.js
+++ api/SparkPost/index.js
@@ -1,6 +1,8 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request to SparkPost Api.');
 
+    const request = require('request');
+
     const SparkPost = require('sparkpost');
     const client = new SparkPost("7f47b822bbffacdd0eb626fff07688523bc8f8f3", { origin: 'https://api.eu.sparkpost.com:443' });
 
@@ -9,53 +11,92 @@ module.exports = async function (context, req) {
 
     const mailAddress = (req.query.mailAddress || (req.body && req.body.mailAddress));
     const mailAddresses = Object.values(mailAddress);
-    // const fileName = (req.query.fileName || (req.body && req.body.fileName));
-    // const fileUrl = (req.query.fileUrl || (req.body && req.body.fileUrl));
+    const fileName = (req.query.fileName || (req.body && req.body.fileName));
+    const fileUrl = (req.query.fileUrl || (req.body && req.body.fileUrl));
     const subject = (req.query.subject || (req.body && req.body.subject));
     const text = (req.query.text || (req.body && req.body.text));
 
     let responseMessage = '';
     let status = 0;
 
-    context.log(mailAddress);
-    context.log(mailAddresses);
-    
+    async function emailWithAtt() {
+        request(fileUrl, { encoding: null }, (error, res, body) => {
+            if (error) {return error; }
+            if (body) {
+                const textBuffered = Buffer.from(body);
+                client.transmissions.send({
+                    options: {
+                    sandbox: false,
+                    click_tracking: false,
+                    },
+                    content: {
+                        from: {
+                            email: 'post@bevardovrefjell.no',
+                            name: 'Bevar Dovrefjell mellom istidene'
+                        },
+                    subject: subject,
+                    html:'<html><body><p>' + text + '</p></body></html>',
+                    attachments: [
+                        {
+                            name: fileName,
+                            type: 'application/pdf',
+                            data: textBuffered.toString('base64')
+                        }
+                    ]
+                    },
+                    recipients: mailAddresses,
+                })
+                .then(responseData => {
+                    status = 200;
+                    responseMessage = responseData;
+                })
+                .catch(err => {
+                    responseMessage = err;
+                    status = 400;
+                });
+                return textBuffered;
+            }
+        });
+    };
 
-async function testing() {
-    await client.transmissions.send({
-        options: {
-        sandbox: false
-        },
-        content: {
-        from: 'post@bevardovrefjell.no',
-        name: 'Bevar Dovrefjell mellom istidene',
-        subject: subject,
-        html:'<html><body><p>' + text + '</p></body></html>'
-        },
-        recipients: mailAddresses
-    })
-    .then(data => {
-        status = 200;
-        responseMessage = data;
-        context.log(data);
-    })
-    .catch(err => {
-        responseMessage = err;
-        status = 400;
-        context.log(err);
-    });
-};
-
-    await testing();
-
-
-
+    async function email() {
+        await client.transmissions.send({
+            options: {
+            sandbox: false,
+            click_tracking: false,
+            },
+            content: {
+            from: {
+                email: 'post@bevardovrefjell.no',
+                name: 'Bevar Dovrefjell mellom istidene'
+            },
+            subject: subject,
+            html:'<html><body><p>' + text + '</p></body></html>'
+            },
+            recipients: mailAddresses
+        })
+        .then(data => {
+            status = 200;
+            responseMessage = data;
+            context.log(data);
+        })
+        .catch(err => {
+            responseMessage = err;
+            status = 400;
+            context.log(err);
+        });
+    };
 
 
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    if (fileName) {
+        await emailWithAtt();
+        context.log('Med vedlegg');
+    }
+    else {
+        context.log('uten vedlegg');
+        await email();
+    };
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
